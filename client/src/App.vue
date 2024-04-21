6aeb6692bb0189eadbca5d8c383483970b9325e7
marlon nino <mvnino.halcyondigital@gmail.com>
2024-04-22 00:56:49 +0800

    feat: update some in client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { Button } from '@/components/ui/button';
 import NavbarMenu from '@/components/NavbarMenu.vue';
-import CmsLayout from './components/CmsLayout.vue';
 </script>
 
-<!-- <template>
+<template>
   <header>
     <NavbarMenu />
   </header>
 
   <RouterView />
-</template> -->
-
-<template>
-  <CmsLayout />
-
-  <!-- Main Content Area -->
-  <main class="flex-1 bg-gray-100 mt-16 md:mt-0">
-    <RouterView />
-  </main>
 </template>
